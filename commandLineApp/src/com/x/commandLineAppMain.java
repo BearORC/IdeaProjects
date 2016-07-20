43498f468c2d77c1df42dd5299de67e914a125a8
bearorc <bearorc@gmail.com>
2016-07-20 11:50:08 +0800

    init commandlineapp

---
+++ commandLineApp/src/com/x/commandLineAppMain.java
@@ -0,0 +1,10 @@
+package com.x;
+
+import sun.applet.Main;
+
+
+public class commandLineAppMain {
+    public static  void main(String args[]){
+        System.out.println("Hello World!!!");
+    }
+}
